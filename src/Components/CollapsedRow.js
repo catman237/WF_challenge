a99dcb4ac9f25611e1db0f075409d1c2898c9b76
Greg Hughes <58540291+catman237@users.noreply.github.com>
2021-11-12 15:48:32 -0700

    finished pagination

--- src/Components/CollapsedRow.js
+++ src/Components/CollapsedRow.js
@@ -6,19 +6,14 @@ import IconButton from "@mui/material/IconButton";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import { Tab } from "@mui/material";
-
 
 const CollapsedRow = ({ item }) => {
-    console.log(item)
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <TableRow>
@@ -34,11 +29,6 @@ const CollapsedRow = ({ item }) => {
         <TableCell component="th" scope="row">
           {item.name}
         </TableCell>
-        {/* <TableCell align="right">{item.customer_last_name}</TableCell>
-        <TableCell align="right">{item.customer_id}</TableCell>
-        <TableCell align="right">{item.order_id}</TableCell>
-        <TableCell align="right">{item.order_amount}</TableCell>
-        <TableCell align="right">{item.order_date}</TableCell> */}
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -57,12 +47,12 @@ const CollapsedRow = ({ item }) => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>{item.id}</TableCell>
-                        <TableCell>{item.description}</TableCell>
-                        <TableCell>{`$${item.price}`}</TableCell>
-                        <TableCell>{item.quantity}</TableCell>
-                    </TableRow>
+                  <TableRow>
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>{item.description}</TableCell>
+                    <TableCell>{`$${item.price}`}</TableCell>
+                    <TableCell>{item.quantity}</TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </Box>
--- src/Components/CustomerTable.js
+++ src/Components/CustomerTable.js
@@ -1,14 +1,28 @@
-import * as React from "react";
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
 import Row from "./Row";
+import { TablePagination } from "@mui/material";
 
 export default function CollapsibleTable({ items }) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(event.target.value, 10);
+    setPage(0);
+  };
+
   return (
     <div>
       <TableContainer component={Paper}>
@@ -25,11 +39,24 @@ export default function CollapsibleTable({ items }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {items.map((item) => {
-              return <Row item={item} />;
-            })}
+            {items
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item) => {
+                return <Row item={item} />;
+              })}
           </TableBody>
         </Table>
+        <TableFooter>
+          <TablePagination
+            rowsPerPageOptions={[1, 5, items.length]}
+            component="div"
+            count={items.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </TableFooter>
       </TableContainer>
     </div>
   );
